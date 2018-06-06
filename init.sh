52dfa9d710897646712321988cab443f244bc353
leandroamore <leandro.amore@prisma.cc>
2018-06-05 21:38:00 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo yum install -y epel-release
 sudo yum install -y stress
 #crontab -l > mycron si quisiera conservar los crons
 echo "5 * * * * /usr/bin/stress -t 1800 -c 2" > mycron
+echo "@reboot /usr/bin/stress -t 1800 -c 2" >> mycron
 crontab mycron
