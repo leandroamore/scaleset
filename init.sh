5759ec6c5648807cd76dfd6738c29b76e6906ffb
leandroamore <leandro.amore@prisma.cc>
2018-06-06 16:36:00 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@
 #crontab -l > mycron si quisiera conservar los crons
 wget ftp://fr2.rpmfind.net/linux/dag/redhat/el7/en/x86_64/dag/RPMS/stress-1.0.2-1.el7.rf.x86_64.rpm 
 sudo yum localinstall stress-1.0.2-1.el7.rf.x86_64.rpm -y
-echo "5 * * * * /usr/bin/stress -t 1800 -c 4" > mycron
+echo "5 * * * * /usr/bin/stress -t 1800 -c 8" > mycron
 crontab mycron
-stress -t 900 -c 4
+stress -t 900 -c 8
