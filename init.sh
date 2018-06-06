aa1a309220c5dd26af39ba541813510060708600
leandroamore <leandro.amore@prisma.cc>
2018-06-06 14:21:16 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo yum install -y epel-release
-sudo yum update
-sudo yum install -y stress
+#sudo yum install -y epel-release
+#sudo yum update
+#sudo yum install -y stress
 #crontab -l > mycron si quisiera conservar los crons
+wget ftp://fr2.rpmfind.net/linux/dag/redhat/el7/en/x86_64/dag/RPMS/stress-1.0.2-1.el7.rf.x86_64.rpm 
+sudo yum localinstall stress-1.0.2-1.el7.rf.x86_64.rpm 
 echo "5 * * * * /usr/bin/stress -t 1800 -c 2" > mycron
 crontab mycron
 stress -t 900 -c 2
