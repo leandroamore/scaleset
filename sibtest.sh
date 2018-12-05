72e1b2d5312d5c451e4c59397e120fe3174bb710
leandroamore <leandro.amore@prisma.cc>
2018-12-05 01:40:42 -0300

    Update sibtest.sh

--- sibtest.sh
+++ sibtest.sh
@@ -3,6 +3,7 @@
 
 sudo yum update
 #creo carpetas y symbolic links
+echo "Creando Symbolic Links"
 sudo mkdir /mnt/shared/modernizacion/test/log/$HOSTNAME
 sudo ln -s /var/log/vuonboarding/ /mnt/shared/modernizacion/test/log/$HOSTNAME
 sudo ln -s /var/log/vudocumentprocessor/ /mnt/shared/modernizacion/test/log/$HOSTNAME
@@ -12,13 +13,16 @@ sudo ln -s /var/log/nginx/ /mnt/shared/modernizacion/test/log/$HOSTNAME
 sudo ln -s /var/log/tomcat-backend/ /mnt/shared/modernizacion/test/log/$HOSTNAME
 sudo ln -s /var/log/tomcat-frontend/ /mnt/shared/modernizacion/test/log/$HOSTNAME
 #reemplazo node.conf
+echo "Cambiando config de node.conf"
 sudo sed -i 's/Default 9/default '$HOSTNAME'/gi' /mnt/shared/modernizacion/test/api-gateway/nginx/node.conf
 #configuro swap en la imagen
+echo "Modificando /etc/waagent.conf"
 sudo sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' /etc/waagent.conf
 sudo sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
-sudo sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=8192/g' /etc/waagent.conf
+sudo sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=4096/g' /etc/waagent.conf
+echo "reiniciando el servicio de Waagent"
 sudo systemctl restart waagent.service
-
-#sudo cd /mnt/shared/modernizacion/test
-#sudo docker-compose  pull --include-deps
+echo "esperando 10 segundos"
+sleep 10s
+echo "levantando dockers"
 sudo docker-compose -f /mnt/shared/modernizacion/test/docker-compose.yml up -d
