dd41ca5a462ab07738e193373e890abab258daf2
leandroamore <leandro.amore@prisma.cc>
2018-12-04 23:54:58 -0300

    Update sibtest.sh

--- sibtest.sh
+++ sibtest.sh
@@ -13,6 +13,11 @@ sudo ln -s /var/log/tomcat-backend/ /mnt/shared/modernizacion/test/log/$HOSTNAME
 sudo ln -s /var/log/tomcat-frontend/ /mnt/shared/modernizacion/test/log/$HOSTNAME
 #reemplazo node.conf
 sudo sed -i 's/Default 9/default '$HOSTNAME'/gi' /mnt/shared/modernizacion/test/api-gateway/nginx/node.conf
+#configuro swap en la imagen
+sudo sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' /etc/waagent.conf
+sudo sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
+sudo sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=8192/g' /etc/waagent.conf
+sudo service walinuxagent restart
 
 sudo cd /mnt/shared/modernizacion/test
 sudo docker-compose  pull --include-deps
